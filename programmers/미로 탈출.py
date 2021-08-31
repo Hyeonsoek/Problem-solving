5ad77d5abc3a726ca71355e3bf44120dd64c2311
Hyeonseok <a2367879@gmail.com>
2021-08-31 21:57:09 +0900

    2021/08/31

--- programmers/미로 탈출.py
+++ programmers/미로 탈출.py
@@ -1,4 +1,4 @@
-# 미완성 코드, 보면서 공부 중
+# https://sklubmk.github.io/2021/07/15/28bed7b50dc1/ 참고...
 
 import heapq
 from collections import defaultdict
@@ -7,34 +7,56 @@ def solution(n, start, end, roads, traps):
     
     traps_dict = {trap-1:idx for idx, trap in enumerate(traps)}
     board = [[] for _ in range(n)]
-    check = [[0] * n for _ in range(1<<len(traps))]
+    check = [[False] * n for _ in range(1<<len(traps))]
     
     for s, e, cost in roads:
         board[s-1].append((e-1, cost, 0))
         board[e-1].append((s-1, cost, 1))
 
-    heap = [(0, start, 0)]
+    heap = [(0, start-1, 0)]
 
     while heap:
         cost, vertex, state = heapq.heappop(heap)
 
         next_state = state
-        isTrap = True if vertex in traps else False
+        isTrap = True if vertex in traps_dict else False
 
-        if vertex == end:
+        if vertex == end-1:
             return cost
 
-        if check[state][vertex] == 1:
+        if check[state][vertex]:
             continue
         else:
-            check[start][vertex] = True
+            check[state][vertex] = True
 
         for next_vertex, next_cost, road_type in board[vertex]:
-            next_isTrap = True if (next_vertex+1) in traps else False
 
-            if not next_isTrap:
+            next_state = state
+            next_isTrap = True if next_vertex in traps_dict else False
+
+            if not (isTrap or next_isTrap):
+                if road_type == 1:
+                    continue
+            elif isTrap and next_isTrap:
+                nowisTrapOn = (state & (1 << traps_dict[vertex])) >> traps_dict[vertex]
+                nextisTrapOn = (state & (1 << traps_dict[next_vertex])) >> traps_dict[next_vertex]
+
+                if (nowisTrapOn ^ nextisTrapOn) != road_type:
+                    continue
+            else:
+                index = vertex if isTrap else next_vertex
+                isTrapOn = (state & (1 << traps_dict[index])) >> traps_dict[index]
+
+                if isTrapOn != road_type:
+                    continue
+
+            if next_isTrap:
+                next_state = state ^ (1 << traps_dict[next_vertex])
+
+            heapq.heappush(heap, (cost + next_cost, next_vertex, next_state))
+
+    return -1
 
 
-
-# print(solution(3, 1, 3, [[1, 2, 2], [3, 2, 3]], [2]))
-# print(solution(4, 1, 4, [[1, 2, 1], [3, 2, 1], [2, 4, 1]], [2, 3]))
+print(solution(3, 1, 3, [[1, 2, 2], [3, 2, 3]], [2]))
+print(solution(4, 1, 4, [[1, 2, 1], [3, 2, 1], [2, 4, 1]], [2, 3]))
--- programmers/블록 이동하기.py
+++ programmers/블록 이동하기.py
@@ -1,43 +1,55 @@
-## 다시푸는중~
-
-from queue import PriorityQueue
+from collections import deque
+from collections import defaultdict
 
 def is_in_board(size, x, y):
     return 0 <= x < size and 0 <= y < size
+
+def loc_string(p):
+    return '.'.join(list(map(str, p)))
 
 def bfs(board):
 
     min_cost = 0
     size = len(board)
 
-    check = [[0] * size for _ in range(size)]
     dirr = [[0,1],[0,-1], [1,0],[-1,0]]
-    
-    pq = PriorityQueue()
-    pq.put((0,(0,0),(0,1))) #cost, 1, 2 position
-    
-    while not pq.empty():
-        cost, p1, p2 = pq.get()
-        x1, y1 = p1
-        x2, y2 = p2
-        
+
+    check = defaultdict(int)
+    pq = deque()
+    pq.append((0,[0,0,0,1])) #cost, 1, 2 position
+    check[loc_string([0, 0, 0, 1])] = 1
+
+    while pq:
+        cost, p = pq.popleft()
+        x1, y1, x2, y2 = p
+
         if (x1 == size - 1 and y1 == size - 1) or \
             (x2 == size - 1 and y2 == size -1):
-                min_cost = cost
-                break
-        
+                return cost
+
         for xdir, ydir in dirr:
             xx1, yy1 = x1 + xdir, y1 + ydir
             xx2, yy2 = x2 + xdir, y2 + ydir
             if is_in_board(size, xx1, yy1) \
-                and is_in_board(size, xx2, yy2) \
-                and board[xx1][yy1] == 0 and board[xx2][yy2] == 0:
-                    board[xx1][yy1] = board[xx2][yy2] = 1
-                    pq.put((cost + 1, (xx1, yy1), (xx2, yy2)))
-        
+                    and is_in_board(size, xx2, yy2) \
+                    and board[xx1][yy1] == 0 and board[xx2][yy2] == 0:
 
+                string = loc_string([xx1, yy1, xx2, yy2])
+                if check[string] == 0:
+                    check[string] = 1
+                    pq.append((cost + 1, [xx1, yy1, xx2, yy2]))
 
-    return min_cost
+                string = loc_string([x1, y1, xx1, yy1])
+                if check[string] == 0:
+                    check[string] = 1
+                    pq.append((cost + 1, [x1, y1, xx1, yy1]))
+
+                string = loc_string([xx2, yy2, x2, y2])
+                if check[string] == 0:
+                    check[string] = 1
+                    pq.append((cost + 1, [xx2, yy2, x2, y2]))
+
+    return -1
 
 def solution(board):
     answer = bfs(board)
