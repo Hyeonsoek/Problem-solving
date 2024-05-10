7d96d62aee3e0b1015652bdc9e1e3d837f33994c
Hyeonseok <a2367879@gmail.com>
2024-05-10 12:48:16 +0900

    2024/05/10

---
+++ boj/20000/24482.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(200000)
+input = sys.stdin.readline
+
+def solve():
+    n, m, r = map(int, input().split())
+    graph = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        u, v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+        
+    for x in range(1, n + 1):
+        graph[x].sort(reverse=True)
+    
+    visited = [-1] * (n + 1)
+    
+    def dfs(node, height):
+        visited[node] = height
+        for nbr in graph[node]:
+            if visited[nbr] == -1:
+                dfs(nbr, height + 1)
+    
+    dfs(r, 0)
+    print(*visited[1:], sep='\n')
+
+solve()
