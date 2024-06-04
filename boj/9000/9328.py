73f58b29acb625934d052dbe6f23f58f3a456e1c
Hyeonseok <a2367879@gmail.com>
2024-06-04 12:47:21 +0900

    2024/06/04

--- boj/9000/9328.py
+++ boj/9000/9328.py
@@ -1,82 +1,63 @@
 import sys
-import copy
-
 from collections import deque
-from collections import defaultdict
+input = sys.stdin.readline
+dirr = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-def to_string(y, x):
-	return str(y) + '/' + str(x)
+def solve():
+    H, W = map(int, input().split())
+    board = [['.'] * (W + 2) for _ in range(H + 2)]
+    
+    for x in range(1, H + 1):
+        line = input().strip()
+        for y in range(1, W + 1):
+            board[x][y] = line[y-1]
+    
+    keys = set(input().strip())
+    document = set()
+    
+    documentSize, keySize = 0, len(keys)
+    
+    while True:
+        queue = deque([(0, 0)])
+        visited = [[0] * (W+2) for _ in range(H+2)]
+        visited[0][0] = 1
+        
+        while queue:
+            y, x = queue.popleft()
+            
+            for ydir, xdir in dirr:
+                yy = y + ydir
+                xx = x + xdir
+                if 0 <= yy < H+2 and 0 <= xx < W+2 \
+                    and board[yy][xx] != '*' and not visited[yy][xx]:
+                
+                    if 'A' <= board[yy][xx] <= 'Z' \
+                            and board[yy][xx].lower() in keys:
+                        visited[yy][xx] = 1
+                        queue.append((yy, xx))
+                        
+                    if 'a' <= board[yy][xx] <= 'z':
+                        visited[yy][xx] = 1
+                        queue.append((yy, xx))
+                        keys.add(board[yy][xx])
+                    
+                    if board[yy][xx] == '.':
+                        visited[yy][xx] = 1
+                        queue.append((yy, xx))
+                        
+                    if board[yy][xx] == '$':
+                        visited[yy][xx] = 1
+                        queue.append((yy, xx))
+                        document.add((yy, xx))
+        
+        if keySize == len(keys) and documentSize == len(document):
+            break
+        
+        keySize = len(keys)
+        documentSize = len(document)
+            
+    return len(document)
 
-def bfs():
-
-	answer = 0
-	pre_check = None
-	key = defaultdict(int)
-	docoment = defaultdict(int)
-
-	dirr = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-	make_check = lambda : [[-1] * (W+2) for _ in range(H+2)]
-
-	while True:
-		q = deque()
-		check = make_check()
-
-		q.append((0, 0))
-		check[0][0] = 1
-
-		while q:
-			y, x = q.popleft()
-
-			for ydir, xdir in dirr:
-				yy = y + ydir
-				xx = x + xdir
-				if 0 <= yy < H+2 and 0 <= xx < W+2 \
-						and board[yy][xx] != '*' and check[yy][xx] == -1:
-
-					string = to_string(yy,xx)
-
-					if ord('A') <= ord(board[yy][xx]) <= ord('Z') \
-							and board[yy][xx].lower() in keys:
-						q.append((yy, xx))
-						check[yy][xx] = 1
-
-					if ord('a') <= ord(board[yy][xx]) <= ord('z'):
-						if key[string] == 0:
-							key[string] = 1
-							keys.append(board[yy][xx])
-						check[yy][xx] = 1
-						q.append((yy, xx))
-
-					if board[yy][xx] == '.':
-						check[yy][xx] = 1
-						q.append((yy, xx))
-
-					if board[yy][xx] == '$':
-						if docoment[string] == 0:
-							answer += 1
-							docoment[string] = 1
-						check[yy][xx] = 1
-						q.append((yy, xx))
-
-		if pre_check and pre_check == check:
-			break
-
-		pre_check = copy.deepcopy(check)
-
-	return answer
-
-answer = []
 T = int(input())
-
-for _ in range(T):
-	H, W = map(int, input().split())
-	board = [list('.' + sys.stdin.readline().strip() + '.') for _ in range(H)]
-	keys = list(sys.stdin.readline().strip())
-
-	board.insert(0, ['.'] * (W+2))
-	board.append(['.'] * (W+2))
-
-	answer.append(bfs())
-
-for a in answer:
-	print(a)
+for a in range(T):
+    print(solve())
